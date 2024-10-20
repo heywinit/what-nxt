d45878845fe7cfcd7d799bb4173b4e3c0c026403
heywinit <heywinit@gmail.com>
2024-10-20 17:32:38 +0530

    oops

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,8 +9,5 @@ export default {
   DB_USER: process.env.DB_USER,
   DB_PASS: process.env.DB_PASS,
 
-  INFO_WEBHOOK: process.env.INFO_WEBHOOK,
-  ERROR_WEBHOOK: process.env.ERROR_WEBHOOK,
-
   PORT: process.env.PORT || 3000,
 };
